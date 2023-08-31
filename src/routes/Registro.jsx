9bdc43a4d4af39b898191b85679ef39b80665c39
CristianMartinez1398 <109698231+CristianMartinez1398@users.noreply.github.com>
2023-08-31 15:51:07 -0600

    f

--- src/routes/Registro.jsx
+++ src/routes/Registro.jsx
@@ -3,7 +3,13 @@ import React, { Component } from 'react'
 export class Registro extends Component {
   render() {
     return (
-      <input type="color" />
+        <>
+        <label>Hola</label>
+        <input></input>
+        
+        </>
+        
+       
     )
   }
 }
