1b32f5751a43b4f4abaf2a635ab597343ac2652b
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-09-05 20:06:42 -0600

    acttaulizacion en la tabla de usuarios para la creacion

--- src/routes/Usuarios.jsx
+++ src/routes/Usuarios.jsx
@@ -9,6 +9,12 @@ export const Usuarios = () => {
     telefono: "",
     email: "",
     fecha_nacimiento: "",
+    password: "",
+    secondPassword: "",
+  });
+
+  const [state, setstate] = useState({
+    error: ''
   });
 
   const handleChange = (event) => {
@@ -30,11 +36,30 @@ export const Usuarios = () => {
 
       if (response.ok) {
         const responseData = await response.json();
+<<<<<<< Updated upstream
         getDatos();
+=======
+        console.log("Datos Enviados");
+        console.log(JSON.stringify(responseData));
+        fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then((data) => setData(data)) 
+          .catch((error) => console.error(error));
+>>>>>>> Stashed changes
       } else {
         const responseBody = await response.json();
         console.log("Error al enviar datos");
         console.log(responseBody);
+
+        setstate({...state, error: responseBody.message})
+        setTimeout(() => {
+          setstate({...state, error: ''})
+        }, 2000);
       }
     } catch (error) {
       console.error("Error al enviar datos", error);
@@ -56,9 +81,21 @@ export const Usuarios = () => {
   }
 
   useEffect(() => {
+<<<<<<< Updated upstream
     
     getDatos();
 
+=======
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setData(data)) // Asegúrate de que este sea el campo correcto
+      .catch((error) => console.error(error));
+>>>>>>> Stashed changes
   }, []);
 
   return (
@@ -66,6 +103,8 @@ export const Usuarios = () => {
       <h1>Usuarios</h1>
 
       <Form onSubmit={handleSubmit}>
+    {state.error ? <div className="notificacion">{state.error}</div> : null }
+
         <Form.Group>
           <Form.Label>Nombre</Form.Label>
           <Form.Control
@@ -106,6 +145,29 @@ export const Usuarios = () => {
           />
         </Form.Group>
 
+        
+        <Form.Group>
+          <Form.Label>Contraseña</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Confirmar Contraseña</Form.Label>
+          <Form.Control
+            type="password"
+            name="secondPassword"
+            value={formData.secondPassword}
+            onChange={(e) => {
+              handleChange(e)
+            }}
+          />
+        </Form.Group>
+
         <Button variant="primary" type="submit">
           Enviar Datos
         </Button>
@@ -124,7 +186,7 @@ export const Usuarios = () => {
           </tr>
         </thead>
         <tbody>
-          {data.map((item) => (
+          {data?.map((item) => (
             <tr key={item.id}>
               <td>{item.id}</td>
               <td>{item.nombre}</td>
