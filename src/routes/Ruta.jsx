6e0979579689a454bbccefb4ee1eb8d2d43a8ee2
Davidleiva504 <105253456+Davidleiva504@users.noreply.github.com>
2023-09-12 13:29:22 -0600

    rutas

--- src/routes/Ruta.jsx
+++ src/routes/Ruta.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button, Table } from 'react-bootstrap';
+import { Form, Button, Table, Modal } from 'react-bootstrap';
 
 const apiUrl = "http://localhost:3000/api/rutas";
 
 export const Rutas = () => {
   const [envio, setEnvio] = useState(0);
+  const [mensaje, setMensaje] = useState(null);
+  const [tipoMensaje, setTipoMensaje] = useState(null);
 
   const [formData, setFormData] = useState({
     string_ruta: '',
@@ -12,14 +14,121 @@ export const Rutas = () => {
     fecha_borrado: null,
   });
 
+  const [rutaSeleccionadaId, setRutaSeleccionadaId] = useState(null);
+  const [rutaSeleccionada, setRutaSeleccionada] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
+
   const cambioData = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const enviarDatos = async (event) => {
-    event.preventDefault();
+  const cambioData1 = (event) => {
+    const { name, value } = event.target;
+    setRutaSeleccionada({ ...rutaSeleccionada, [name]: value });
+  };
 
+  const mostrarMensaje = (mensaje, tipo) => {
+    setMensaje(mensaje);
+    setTipoMensaje(tipo);
+
+    // Borra el mensaje después de unos segundos (opcional)
+    setTimeout(() => {
+      setMensaje(null);
+      setTipoMensaje(null);
+    }, 5000); // Muestra el mensaje durante 5 segundos (puedes ajustarlo según tus preferencias)
+  };
+
+  const handleEditarClick = async (id) => {
+    setRutaSeleccionadaId(id);
+
+    try {
+      const response = await fetch(`${apiUrl}/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        setRutaSeleccionada(responseData.ruta);
+        setModalVisible(true);
+      } else {
+        const responseBody = await response.json();
+        console.error("Error al obtener detalles de la ruta", responseBody);
+      }
+    } catch (error) {
+      console.error("Error al obtener detalles de la ruta", error);
+    }
+  };
+
+  const handleCancelarEdicion = () => {
+    setModalVisible(false);
+  };
+
+  const actualizarRuta = async () => {
+    try {
+      const response = await fetch(`${apiUrl}/${rutaSeleccionadaId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(rutaSeleccionada),
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log("Ruta actualizada exitosamente", responseData);
+        setModalVisible(false);
+        setEnvio(envio + 1);
+
+        // Mostrar mensaje de éxito
+        mostrarMensaje("Ruta actualizada exitosamente", "success");
+      } else {
+        const responseBody = await response.json();
+        console.error("Error al actualizar la ruta", responseBody);
+
+        // Mostrar mensaje de error
+        mostrarMensaje("Error al actualizar la ruta", "error");
+      }
+    } catch (error) {
+      console.error("Error al actualizar la ruta", error);
+
+      // Mostrar mensaje de error
+      mostrarMensaje("Error al actualizar la ruta", "error");
+    }
+  };
+
+  const handleEliminarClick = async (id) => {
+    if (window.confirm("¿Estás seguro de que deseas eliminar esta ruta?")) {
+      try {
+        const response = await fetch(`${apiUrl}/${id}`, {
+          method: 'DELETE',
+        });
+
+        if (response.ok) {
+          console.log("Ruta eliminada exitosamente");
+          setEnvio(envio + 1);
+
+          // Mostrar mensaje de éxito
+          mostrarMensaje("Ruta eliminada exitosamente", "success");
+        } else {
+          console.error("Error al eliminar la ruta:", response.statusText);
+
+          // Mostrar mensaje de error
+          mostrarMensaje("Error al eliminar la ruta", "error");
+        }
+      } catch (error) {
+        console.error("Error al eliminar la ruta", error);
+
+        // Mostrar mensaje de error
+        mostrarMensaje("Error al eliminar la ruta", "error");
+      }
+    }
+  };
+
+  const crearRuta = async () => {
     try {
       const response = await fetch(apiUrl, {
         method: 'POST',
@@ -31,60 +140,72 @@ export const Rutas = () => {
 
       if (response.ok) {
         const responseData = await response.json();
-        console.log("Datos de Ruta Enviados");
-        console.log(responseData);
-
+        console.log("Ruta creada exitosamente", responseData);
         setEnvio(envio + 1);
+        setModalVisible(false);
+
+        // Mostrar mensaje de éxito
+        mostrarMensaje("Ruta creada exitosamente", "success");
       } else {
         const responseBody = await response.json();
-        console.log("Error al enviar datos de ruta");
-        console.log(responseBody);
+        console.error("Error al crear la ruta", responseBody);
+
+        // Mostrar mensaje de error
+        mostrarMensaje("Error al crear la ruta", "error");
       }
     } catch (error) {
-      console.error("Error al enviar datos de ruta", error);
+      console.error("Error al crear la ruta", error);
+
+      // Mostrar mensaje de error
+      mostrarMensaje("Error al crear la ruta", "error");
     }
   };
 
-  const [data, setData] = useState([]);
+  const obtenerRutas = async () => {
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-  const obtenerDatos = () => {
-    fetch("http://localhost:3000/api/rutas", { 
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.rutas) {
-          setData(data.rutas);
-        } else {
-          console.error("No se recibieron datos de rutas desde la API");
-        }
-      })
-      .catch((error) => console.error(error));
+      if (response.ok) {
+        const responseData = await response.json();
+        setRutas(responseData.rutas);
+      } else {
+        const responseBody = await response.json();
+        console.error("Error al obtener las rutas", responseBody);
+
+        // Mostrar mensaje de error
+        mostrarMensaje("Error al obtener las rutas", "error");
+      }
+    } catch (error) {
+      console.error("Error al obtener las rutas", error);
+
+      // Mostrar mensaje de error
+      mostrarMensaje("Error al obtener las rutas", "error");
+    }
   };
 
+  const [rutas, setRutas] = useState([]);
+
   useEffect(() => {
-    obtenerDatos(); // Llama a la función para obtener los datos cuando se monta el componente.
-  }, []);
+    obtenerRutas();
+  }, [envio]);
 
   return (
     <>
+      {/* Formulario de Creación */}
       <div className="container mt-2">
         <div className="row">
-          <div className="col-md-12">
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-md-10 offset-md-1">
+          <div className="col-md-8 offset-md-2">
             <div className="card">
               <div className="card-header">
-                Formulario de Registro de Ruta
+                Formulario de Creación de Ruta
               </div>
               <div className="card-body">
-                <Form onSubmit={enviarDatos}>
+                <Form>
                   <Form.Group>
                     <Form.Label>Nombre de Ruta</Form.Label>
                     <Form.Control
@@ -104,51 +225,105 @@ export const Rutas = () => {
                       onChange={() => setFormData({ ...formData, activa: !formData.activa })}
                     />
                   </Form.Group>
-
                   <div className="d-flex justify-content-end">
-                    <Button variant='primary' type='submit'>Enviar Datos de Ruta</Button>
+                    <Button variant="primary" onClick={crearRuta}>
+                      Crear Ruta
+                    </Button>
                   </div>
                 </Form>
               </div>
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="row mt-5">
+      {/* Lista de Rutas */}
+      <div className="container mt-2">
+        <div className="row">
           <div className="col-md-12">
-            <h1 className="mb-4">Reporte de Rutas</h1>
+            <h1 className="mb-4">Lista de Rutas</h1>
+            {mensaje && (
+              <Mensaje mensaje={mensaje} tipo={tipoMensaje} />
+            )}
             <Table striped bordered hover>
               <thead>
                 <tr>
-                  <th>#Ruta</th>
+                  <th>#</th>
                   <th>Nombre de Ruta</th>
                   <th>Ruta Activa</th>
-                  <th>Fecha de Borrado</th>
+                  <th>Acciones</th>
                 </tr>
               </thead>
               <tbody>
-                {data ? (
-                  data.map(item => (
-                    <tr key={item.id}>
-                      <td>{item.id}</td>
-                      <td>{item.string_ruta}</td>
-                      <td>{item.activa ? 'Sí' : 'No'}</td>
-                      <td>{item.fecha_borrado ? item.fecha_borrado : 'N/A'}</td>
-                    </tr>
-                  ))
-                ) : (
-                  <tr>
-                    <td colSpan="4">Cargando data...</td>
+                {rutas.map((ruta) => (
+                  <tr key={ruta.id}>
+                    <td>{ruta.id}</td>
+                    <td>{ruta.nombre_ruta}</td>
+                    <td>{ruta.activa ? 'Sí' : 'No'}</td>
+                    <td>
+                      <Button variant="info" onClick={() => handleEditarClick(ruta.id)}>Editar</Button>{' '}
+                      <Button variant="danger" onClick={() => handleEliminarClick(ruta.id)}>Eliminar</Button>
+                    </td>
                   </tr>
-                )}
+                ))}
               </tbody>
             </Table>
           </div>
         </div>
       </div>
+
+      {/* Modal de Edición */}
+      <Modal show={modalVisible} onHide={handleCancelarEdicion}>
+        <Modal.Header closeButton>
+          <Modal.Title>Editar Ruta</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>Nombre de Ruta</Form.Label>
+              <Form.Control
+                type='text'
+                name='string_ruta'
+                value={rutaSeleccionada ? rutaSeleccionada.string_ruta : ''}
+                onChange={cambioData1}
+              />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Ruta Activa</Form.Label>
+              <Form.Check
+                type="checkbox"
+                name="activa"
+                checked={rutaSeleccionada.activa || false}
+                onChange={() => setRutaSeleccionada({ ...rutaSeleccionada, activa: !rutaSeleccionada.activa })}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCancelarEdicion}>
+            Cancelar
+          </Button>
+          <Button variant="primary" onClick={actualizarRuta}>
+            Guardar Cambios
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
 
-export default Rutas;
+const Mensaje = ({ mensaje, tipo }) => {
+  if (!mensaje) return null;
 
+  let className;
+  if (tipo === "success") {
+    className = "alert alert-success";
+  } else if (tipo === "error") {
+    className = "alert alert-danger";
+  }
+
+  return <div className={className}>{mensaje}</div>;
+};
+
+export default Rutas;
