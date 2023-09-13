035ab34bf664386f216f3ae3e972e3233ecd7ce8
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-09-12 20:23:51 -0600

    usuarios editar

--- src/routes/Usuarios.jsx
+++ src/routes/Usuarios.jsx
@@ -1,5 +1,6 @@
 import  { useState, useEffect } from "react";
 import { Form, Button, Table } from "react-bootstrap";
+import Modal from "../components/modal";
 
 const url = "http://localhost:3000/api/usuarios";
 
@@ -15,7 +16,15 @@ export const Usuarios = () => {
 
   const [state, setstate] = useState({
     error: null,
-    success: null
+    success: null,
+    modalIsOpen: false,
+    selectedUser: {
+      editnombre: "",
+      edittelefono: "",
+      editemail: "",
+      editpassword: "",
+      editsecondPassword: "",
+    }
   });
 
   const handleChange = (event) => {
@@ -23,8 +32,41 @@ export const Usuarios = () => {
     setFormData({ ...formData, [name]: value });
   };
 
+  const handleEditChange = (event) => {
+    const { name, value } = event.target;
+    setstate((previous) => ({...previous, selectedUser: {...previous.selectedUser, [name]: value}}))
+  }
+  
+
+  
+  const handleOpenModal = () => {
+    setstate((previous) => ({...previous, modalIsOpen: true}));
+  };
+
+  const handleCloseModal = () => {
+    setstate( previous => ({...previous, modalIsOpen: false}) );
+  };
+
+
+  
+  const changeSelectedUser = (user) => {
+    setstate((previous => ({...previous, selectedUser: {
+      editnombre: user.nombre,
+      edittelefono: user.telefono,
+      editemail: user.email,
+      editfecha_nacimiento: user.fecha_nacimiento,
+      editpassword: user.password,
+      editsecondPassword: user.secondPassword,
+      id: user.id
+
+    } })))
+  }
+
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+
 
     try {
       const response = await fetch(url, {
@@ -36,8 +78,7 @@ export const Usuarios = () => {
       });
 
       if (response.ok) {
-        const responseData = await response.json();
-        setstate({...state, success: responseData.message})
+        setstate({...state, success: 'Usuario creado con exito'})
 
         setFormData({
           nombre: "",
@@ -66,16 +107,77 @@ export const Usuarios = () => {
 
         setstate({...state, error: responseBody.message})
         setTimeout(() => {
-          setstate({...state, error: null})
+          setstate(  previous => ({...previous, error: null}) );
         }, 2000);
       }
     } catch (error) {
-      setstate({...state, error: 'Ha ocurrido un error'})
+      setstate(previous => ({...previous, error: "Ha ocurrido un error"}) );
     }
   };
 
   const [data, setData] = useState([]);
 
+
+  const handleSubmitEdit = async (event) => {
+    event.preventDefault();
+  
+    const userId = state.selectedUser?.id;
+
+    const data = {
+      nombre: state.selectedUser?.editnombre,
+      telefono: state.selectedUser?.edittelefono,
+      email: state.selectedUser?.editemail,
+      password: state.selectedUser?.editpassword,
+    }
+
+    console.log(data)
+  
+    try {
+      const response = await fetch(`${url}/${userId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+  
+      if (response.ok) {
+        setstate(previous => ({...previous, success: "Usuario actualizado con exito"}));
+        handleCloseModal();
+        setFormData({
+          nombre: "",
+          telefono: "",
+          email: "",
+          fecha_nacimiento: "",
+          password: "",
+          secondPassword: "",
+        });
+        fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then((response) => {
+            setData(response.data)
+          }) 
+          .catch((error) => console.error(error));
+
+
+      } else {
+        const responseBody = await response.json();
+        setstate({ ...state, error: responseBody.message });
+
+        setTimeout(() => {
+          setstate({ ...state, error: null });
+        }, 2000);
+      }
+    } catch (error) {
+      setstate({ ...state, error: "Ha ocurrido un error" });
+    }
+  };
+  
 
 
   useEffect(() => {
@@ -96,6 +198,66 @@ export const Usuarios = () => {
 
   return (
     <>
+ <Modal isOpen={state.modalIsOpen} onClose={handleCloseModal}>
+      <h1>Editar Usuario</h1>
+      <Form onSubmit={handleSubmitEdit}>
+  <Form.Group>
+    <Form.Label>Nombre</Form.Label>
+    <Form.Control
+      type="text"
+      name="editnombre"
+      value={state.selectedUser.editnombre}
+      onChange={handleEditChange}
+    />
+  </Form.Group>
+
+  <Form.Group>
+    <Form.Label>Teléfono</Form.Label>
+    <Form.Control
+      type="text"
+      name="edittelefono"
+      value={state.selectedUser.edittelefono}
+      onChange={handleEditChange}
+    />
+  </Form.Group>
+
+  <Form.Group>
+    <Form.Label>Email</Form.Label>
+    <Form.Control
+      type="email"
+      name="editemail"
+      value={state.selectedUser.editemail}
+      onChange={handleEditChange}
+    />
+  </Form.Group>
+
+
+  <Form.Group>
+    <Form.Label>Contraseña</Form.Label>
+    <Form.Control
+      type="password"
+      name="editpassword"
+      value={state.selectedUser.editpassword}
+      onChange={handleEditChange}
+    />
+  </Form.Group>
+
+  <Form.Group>
+    <Form.Label>Confirmar Contraseña</Form.Label>
+    <Form.Control
+      type="password"
+      name="editsecondPassword"
+      value={state.selectedUser.editsecondPassword}
+      onChange={handleEditChange}
+    />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+    Actualizar Usuario
+  </Button>
+</Form>
+
+    </Modal>
       <h1>Usuarios</h1>
 
       <Form onSubmit={handleSubmit}>
@@ -128,16 +290,6 @@ export const Usuarios = () => {
             type="email"
             name="email"
             value={formData.email}
-            onChange={handleChange}
-          />
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label>Fecha de Nacimiento</Form.Label>
-          <Form.Control
-            type="date"
-            name="fecha_nacimiento"
-            value={formData.fecha_nacimiento}
             onChange={handleChange}
           />
         </Form.Group>
@@ -190,6 +342,14 @@ export const Usuarios = () => {
               <td>{item.telefono}</td>
               <td>{item.email}</td>
               <td>{item.fecha_nacimiento}</td>
+              <td>
+                <Button onClick={() => {
+            
+              handleOpenModal();
+
+              changeSelectedUser(item);
+
+                }}>Editar</Button></td>
             </tr>
           ))}
         </tbody>
